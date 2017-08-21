c645fe1af17be86fc8a80e07c994b66de2368143
bzub <bzubrod@gmail.com>
2017-08-21 17:28:45 -0500

    Add api_servers_ips input variable for IP SAN

--- tls-k8s.tf
+++ tls-k8s.tf
@@ -73,9 +73,7 @@ resource "tls_cert_request" "apiserver" {
     "kubernetes.default.svc.cluster.local",
   ]
 
-  ip_addresses = [
-    "${cidrhost(var.service_cidr, 1)}",
-  ]
+  ip_addresses = "${distinct([cidrhost(var.service_cidr, 1)],compact(var.api_servers_ips))}"
 }
 
 resource "tls_locally_signed_cert" "apiserver" {
